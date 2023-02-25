555e4eb68149dde21c053205ab29364356b51a31
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-02-26 06:19:17 +0800

    准备打包构建

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='momotou',
+    version='1.0.0',
+    packages=['momotou'],
+    url='https://www.qsnctf.com/',
+    license='',
+    author='Moxin',
+    author_email='1044631097@qq.com',
+    description='摸摸头表情包制作'
+)
