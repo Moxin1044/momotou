31634a29a674e81dfcb21f778f4b467836b6bf40
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-02-26 06:23:35 +0800

    准备打包构建

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import io
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='momotou',
     version='1.0.0',
     packages=['momotou'],
+    install_requires=['Pillow'],
     url='https://www.qsnctf.com/',
+    package_data={'momotou': ['data/*']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='',
     author='Moxin',
     author_email='1044631097@qq.com',
